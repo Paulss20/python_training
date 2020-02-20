8211981f64cf45ce8f938ecbcd506e9549a1efca
sergei_sh <pazoviy@ayndex.ru>
2020-02-20 17:41:26 +0300

    Успешный запуск теста test_add_group.py в Pycharm

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
