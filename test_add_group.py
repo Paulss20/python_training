c064644525556d1a671646a033c08aacd3917127
sergei_sh <pazoviy@ayndex.ru>
2020-02-25 14:20:11 +0300

    Создаеи вспомогательные методы в скрипт test_add_group, как у Алексея в первом уроке (Создание вспомогательных методов...). Повышена чмтаемость кода.

--- test_add_group.py
+++ test_add_group.py
@@ -16,35 +16,48 @@ class test_add_group(unittest.TestCase):
 
     def test_test_add_group(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("rrrrrrrrrrttyty")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("wewrtdycuv")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("xcvbnmghj")
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        # return to groups page
-        wd.find_element_by_link_text("group page").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+         wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+         wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
+         # init group creation
+         wd.find_element_by_name("new").click()
+         # fill group firm
+         wd.find_element_by_name("group_name").click()
+         wd.find_element_by_name("group_name").clear()
+         wd.find_element_by_name("group_name").send_keys("rrrrrrrrrrttyty")
+         wd.find_element_by_name("group_header").click()
+         wd.find_element_by_name("group_header").clear()
+         wd.find_element_by_name("group_header").send_keys("wewrtdycuv")
+         wd.find_element_by_name("group_footer").click()
+         wd.find_element_by_name("group_footer").clear()
+         wd.find_element_by_name("group_footer").send_keys("xcvbnmghj")
+         # submit group creation
+         wd.find_element_by_name("submit").click()
+
+    def open_groups_page(self, wd):
+         wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+         wd.find_element_by_name("user").click()
+         wd.find_element_by_name("user").clear()
+         wd.find_element_by_name("user").send_keys("admin")
+         wd.find_element_by_name("pass").clear()
+         wd.find_element_by_name("pass").send_keys("secret")
+         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+         wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
