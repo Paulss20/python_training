18b32accc0b729aa01d6a0dce5d34dc37d774008
sergei_sh <pazoviy@ayndex.ru>
2020-02-25 11:04:12 +0300

    Измененн скрипт test_add_group как у Алексея в первом уроке (Основные элементы структуры кода). Вынес функцию is_alert_present(wd) за пределы класса test_add_group(unittesr.Test.Case). Значительно сократил import.

--- test_add_group.py
+++ test_add_group.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
-class TestAddGroup(unittest.TestCase):
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
 
-    def test_add_group(self):
+    def test_test_add_group(self):
+        success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
@@ -36,16 +38,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+        self.assertTrue(success)
     
     def tearDown(self):
         self.wd.quit()
