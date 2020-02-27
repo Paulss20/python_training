247989145e8a88a618b988b50eec22e4e9377339
sergei_sh <pazoviy@ayndex.ru>
2020-02-27 12:22:31 +0300

    Задание 3, урок 1. Создан файл add_new.py в PyCharm. Создан класс AddNew. Подключил этот класс в скрипте test_add_new.py строкой from add_new import AddNew.

---
+++ add_new.py
@@ -0,0 +1,22 @@
+
+
+class AddNew:
+     def __init__(self, my_f_name, my_m_name, my_l_name, my_nickname, my_company, work_address, my_h_telefon, my_mobile, my_work_telefon, my_fax, my_company_mail, my_second_mail, my_homepage, my_byear, my_home_address, my_second_address, my_notes):
+          self.my_f_name = my_f_name
+          self.my_m_name = my_m_name
+          self.my_l_name = my_l_name
+          self.my_nickname = my_nickname
+          self.my_company = my_company
+          self.work_address = work_address
+          self.my_h_telefon = my_h_telefon
+          self.my_mobile = my_mobile
+          self.my_work_telefon = my_work_telefon
+          self.my_fax = my_fax
+          self.my_company_mail = my_company_mail
+          self.my_second_mail = my_second_mail
+          self.my_homepage = my_homepage
+          self.my_byear = my_byear
+          self.my_home_address = my_home_address
+          self.my_second_address = my_second_address
+          self.my_notes = my_notes
+
