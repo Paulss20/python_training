513b735c06e1d2bc97f31b61456b539487145cc2
sergei_sh <pazoviy@ayndex.ru>
2020-03-03 15:08:38 +0300

    Урок 2. Задание 4. Изменения в файле test_add_new.py в PyCharm (Видео урок - "Фикстура общий код инициализации и завершения"). 1) Создание самостоятельного обЪекта fixture; 2) Переход на использование pytest; 3) Создание функции app(), которая инициализирует фикстуру (добавление метки @pytest.fixture); 4) Для разрушения фикстуры, в функцию, которая инициализирует фикстуру - app(), передается специальный параметр - request. И у этого параметра есть особый метод - request.addfinalizer(). В него надо передать функцию fixture.destroy. Итого получаем: request.addfinalizer(fixture.destroy). Она будет вызвана автоматически в нужный момент; 5) Удаление тестового класса и функции завершения; 6) Из тестового класса делаем тестовые функции, где в качестве параметра они принимают фикстуру app; 7) Оставляем только утилиту pytest, a unittest удаляем.

--- test_add_new.py
+++ test_add_new.py
@@ -1,44 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.firefox.webdriver import WebDriver
+import pytest
 from add_new import AddNew
-import unittest
 from contactsapp import ContactsApplication
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_new(unittest.TestCase):
-    def setUp(self):
-        self.app = ContactsApplication()
+@pytest.fixture
+def app(request):
+     fixture = ContactsApplication()
+     request.addfinalizer(fixture.destroy)
+     return fixture
 
-    def test_add_new(self):
-        self.app.login(username="admin", password="secret")
-        self.app.fill_add_new_form(AddNew(my_f_name="Sergei", my_m_name="Fedorovich", my_l_name="Semenov", my_nickname="try",
+
+def test_add_new(app):
+    app.login(username="admin", password="secret")
+    app.fill_add_new_form(AddNew(my_f_name="Sergei", my_m_name="Fedorovich", my_l_name="Semenov", my_nickname="try",
                                my_company="Big company", work_address="SPb, Fuchika str, 15", my_h_telefon="11111111",
                                my_mobile="2222222222", my_work_telefon="3333333333", my_fax="4444444444",
                                my_company_mail="big.company@gg.com", my_second_mail="b_company@gg.com",
                                my_homepage="www.big-company.su", my_byear="1985", my_home_address="SPb, Nevski str, 98",
                                my_second_address="SPb, Pushkin, Gogol str, 87", my_notes="fff"))
-        self.app.logout()
+    app.logout()
 
-    def test_add_empty_new(self):
-        self.app.login(username="admin", password="secret")
-        self.app.fill_add_new_form(AddNew(my_f_name="", my_m_name="", my_l_name="", my_nickname="",
+
+def test_add_empty_new(app):
+    app.login(username="admin", password="secret")
+    app.fill_add_new_form(AddNew(my_f_name="", my_m_name="", my_l_name="", my_nickname="",
                                my_company="", work_address="", my_h_telefon="",
                                my_mobile="", my_work_telefon="", my_fax="",
                                my_company_mail="", my_second_mail="",
                                my_homepage="", my_byear="", my_home_address="",
                                my_second_address="", my_notes=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
