002cb540e6d0ba1d06088276957afdd34269f02f
Paulss20 <pazoviy@yandex.ru>
2020-03-25 22:25:42 +0300

    Урок 3. Видео "ПДТ-Python-b12 Повышаем интеллектуальность функций login и logout" Модифицировал тестовые методы: В классе SessionHelper для повышения интеллектуальности входа на сайт и выхода, добавились вспомогательные методы: 1) def is_logged_in(self): 2) def is_logged_in_as(self, username): 3) def ensure_logout(self): 4) def ensure_login(self, username, password): Модифицировали общую фикстуру, добавлением метода ensure_logout(). В классе Application снизили время ожидания с 60 секунд на 5.

--- fixture/session.py
+++ fixture/session.py
@@ -20,4 +20,25 @@ class SessionHelper:
           wd.find_element_by_link_text("Logout").click()
           wd.find_element_by_name("user").click()
 
+     def is_logged_in(self):
+          wd = self.app.wd
+          return len(wd.find_elements_by_link_text("Logout")) > 0
 
+     def is_logged_in_as(self, username):
+          wd = self.app.wd
+          return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"      # xpath as Alexei show
+#          return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"   # xpath as Katalon recorder show
+
+     def ensure_logout(self):
+          wd = self.app.wd
+          if self.is_logged_in():
+               self.logout()
+
+     def ensure_login(self, username, password):
+          wd = self.app.wd
+          if self.is_logged_in():
+               if self.is_logged_in_as(username):
+                    return
+               else:
+                    self.logout()
+          self.login(username, password)
