e2df36bc15837e3e45a74b273a9e6fa4aee84f4c
sergei_sh <pazoviy@ayndex.ru>
2020-04-06 18:04:06 +0300

    Урок 3. Задание 9. Видео - "Повышаем интеллектуальность тестов обеспечение выполнение предусловий"  Модификация файлов для Контактов.  1) Переименование метода filling(AddNew()) в create_contact(AddNew()) через рефактор;  2) Создание метода - count() для подсчета чекбоксов в форме отображения контактов;  3) Модификация теста для удаления первого контакта - def test_delete_first_contact(app):. Теперь метод проверяет есть ли хотя бы один контакт в форме. Если нет, то он его создает с фамилией - "Elephant" и именем - "Mamont".

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -4,18 +4,18 @@ from model.add_new import AddNew
 
 
 def test_add_new(app):
-    app.contacts.filling(AddNew(my_f_name="Sergei", my_m_name="Fedorovich", my_l_name="Semenov", my_nickname="try",
-                                my_company="Big company", work_address="SPb, Fuchika str, 15", my_h_telefon="11111111",
-                                my_mobile="2222222222", my_work_telefon="3333333333", my_fax="4444444444",
-                                my_company_mail="big.company@gg.com", my_second_mail="b_company@gg.com",
-                                my_homepage="www.big-company.su", my_byear="1985", my_home_address="SPb, Nevski str, 98",
-                                my_second_address="SPb, Pushkin, Gogol str, 87", my_notes="fff"))
+    app.contacts.create_contact(AddNew(my_f_name="Sergei", my_m_name="Fedorovich", my_l_name="Semenov", my_nickname="try",
+                                       my_company="Big company", work_address="SPb, Fuchika str, 15", my_h_telefon="11111111",
+                                       my_mobile="2222222222", my_work_telefon="3333333333", my_fax="4444444444",
+                                       my_company_mail="big.company@gg.com", my_second_mail="b_company@gg.com",
+                                       my_homepage="www.big-company.su", my_byear="1985", my_home_address="SPb, Nevski str, 98",
+                                       my_second_address="SPb, Pushkin, Gogol str, 87", my_notes="fff"))
 
 
 def test_add_empty_new(app):
-    app.contacts.filling(AddNew(my_f_name="", my_m_name="", my_l_name="", my_nickname="",
-                                my_company="", work_address="", my_h_telefon="",
-                                my_mobile="", my_work_telefon="", my_fax="",
-                                my_company_mail="", my_second_mail="",
-                                my_homepage="", my_byear="", my_home_address="",
-                                my_second_address="", my_notes=""))
+    app.contacts.create_contact(AddNew(my_f_name="", my_m_name="", my_l_name="", my_nickname="",
+                                       my_company="", work_address="", my_h_telefon="",
+                                       my_mobile="", my_work_telefon="", my_fax="",
+                                       my_company_mail="", my_second_mail="",
+                                       my_homepage="", my_byear="", my_home_address="",
+                                       my_second_address="", my_notes=""))
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,8 @@
+from model.add_new import AddNew
 
 
 def test_delete_first_contact(app):
+     if app.contacts.count() == 0:
+          app.contacts.create_contact(AddNew(my_l_name="Elephant", my_f_name="Mamont"))
      app.contacts.delete_first_contact()
 
