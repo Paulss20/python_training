f72a2f52590effca14a2cae43b44c6fd931cb70d
sergei_sh <pazoviy@ayndex.ru>
2020-07-08 12:35:55 +0300

    Урок 5. Видео - "ПДТ-Python-b12 Режем строки на части делаем тесты для проверки телефонов" :  1) В файл contacts.py добавил импорт для регулярных выражений  2) Там же добавил метод def get_contact_from_view_page(self, index): для взятия контакта со станицы отображения  3) Создан новый метод - "def test_phones_on_contact_view_page(app):" для проверки телефонов на наличие знаков "+", "()", "-" и "пробел". Здесь же добавил импорт для регулярных выражений

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import Select
 from model.add_new import AddNew
+import re
 
 
 class ContactsHelper:
@@ -153,3 +154,14 @@ class ContactsHelper:
           phone2 = wd.find_element_by_name("phone2").get_attribute("value")
           return AddNew(my_f_name=firstname, my_l_name=lastname, my_id=id_contact, my_h_telefon=home_phone,
                          my_mobile=mobile_phone, my_work_telefon=work_phone, my_secondary_phone=phone2)
+
+     def get_contact_from_view_page(self, index):
+          wd = self.app.wd
+          self.open_contact_view_by_index(index)
+          text = wd.find_element_by_id("content").text
+          home_phone = re.search("H: (.*)", text).group(1)
+          work_phone = re.search("W: (.*)", text).group(1)
+          mobile_phone = re.search("M: (.*)", text).group(1)
+          phone2 = re.search("P: (.*)", text).group(1)
+          return AddNew(my_h_telefon=home_phone, my_mobile=mobile_phone,
+                        my_work_telefon=work_phone, my_secondary_phone=phone2)
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,20 @@
-
+import re
 
 def test_phones_on_home_page(app):
      contact_from_home_page = app.contacts.get_contact_list()[0]
      contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(0)
-     assert contact_from_home_page.my_h_telefon == contact_from_edit_page.my_h_telefon
-     assert contact_from_home_page.my_mobile == contact_from_edit_page.my_mobile
-     assert contact_from_home_page.my_work_telefon == contact_from_edit_page.my_work_telefon
-     assert contact_from_home_page.my_secondary_phone == contact_from_edit_page.my_secondary_phone
+     assert contact_from_home_page.my_h_telefon == clear(contact_from_edit_page.my_h_telefon)
+     assert contact_from_home_page.my_mobile == clear(contact_from_edit_page.my_mobile)
+     assert contact_from_home_page.my_work_telefon == clear(contact_from_edit_page.my_work_telefon)
+     assert contact_from_home_page.my_secondary_phone == clear(contact_from_edit_page.my_secondary_phone)
+
+def test_phones_on_contact_view_page(app):
+     contact_from_view_page = app.contacts.get_contact_from_view_page(0)
+     contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(0)
+     assert contact_from_view_page.my_h_telefon == contact_from_edit_page.my_h_telefon
+     assert contact_from_view_page.my_mobile == contact_from_edit_page.my_mobile
+     assert contact_from_view_page.my_work_telefon == contact_from_edit_page.my_work_telefon
+     assert contact_from_view_page.my_secondary_phone == contact_from_edit_page.my_secondary_phone
+
+def clear(s):
+     return re.sub("[() -]", "", s)
