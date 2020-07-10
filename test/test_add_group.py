210a96c2b762d0754edd70136fef9c6e7b862509
sergei_sh <pazoviy@ayndex.ru>
2020-07-10 17:32:30 +0300

    Урок 5. Видео - "7_ПДТ-Python-b12 Параметризация тестов. Data Driven Testing" :  1) В тесте создания групп добавили случайность при создании имени, хедера и футера.  А также разделили тестовые данные - "testdata" и сценарии в тесте def test_add_group(app, group):, правда он пока закомментиррован

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+     symbols = string.ascii_letters + string.digits + " "*10
+     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [
-     Group(name="Gogogo", header="dtdtdtd", footer="bebebebe"),
+     Group(name=random_string("name", 10), header=random_string("header", 15), footer=random_string("footer", 15)),
      Group(name="", header="", footer="")
 ]
 
