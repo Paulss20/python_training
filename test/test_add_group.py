aa3cba11c3a2a370231afbefb0da3746682c25a4
sergei_sh <pazoviy@ayndex.ru>
2020-07-13 12:22:45 +0300

    Урок 5. Видео - "7_ПДТ-Python-b12 Параметризация тестов. Data Driven Testing"  В тесте создания групп можно добавить еще больше случайности в виде символов - "+ string.punctuation"  (в случае, если тесты уже уверенно запускаются с остальными параметрами)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,25 +5,22 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-     symbols = string.ascii_letters + string.digits + " "*10
+     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
      return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [
-     Group(name=name, header=header, footer=footer)
-     for name in ["", random_string("name", 10)]
-     for header in ["", random_string("header", 15)]
-     for footer in ["", random_string("footer", 15)]
+testdata = [Group(name="", header="", footer="")] + [
+     Group(name=random_string("name", 10), header=random_string("header", 15), footer=random_string("footer", 15))
+     for i in range(5)
 ]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
-     pass
-     #old_groups = app.group.get_group_list()
-     #app.group.create(group)
-     #assert len(old_groups) + 1 == app.group.count() # you need to specify not the length but just the value that the count method returns
-     #new_groups = app.group.get_group_list()
-     #old_groups.append(group)
-     #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+     old_groups = app.group.get_group_list()
+     app.group.create(group)
+     assert len(old_groups) + 1 == app.group.count() # you need to specify not the length but just the value that the count method returns
+     new_groups = app.group.get_group_list()
+     old_groups.append(group)
+     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
