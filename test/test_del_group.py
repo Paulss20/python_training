9b304605353418c4c93446473132d0c17222acc1
sergei_sh <pazoviy@ayndex.ru>
2020-07-23 10:30:06 +0300

    Урок 7. Видео "5_ПДТ-Python-b12 Отключаемые проверки пользовательского интерфейса"  1) В тесте "test_del_group.py" добавили отключаемую проверку с помощью флага "check_ui".    Теперь, если надо тест выполнить быстро, то в "Edit configuration..." убираем параметр "--check_ui".    А если надо более тщательно и медленно, то - наоборот, добавляем в "Edit configuration..." параметр "--check_ui".  2) Импортировали пакет для работы с регулярными выражениями. В новом списке "new_groups_ui" избавляемся от "лишних" пробелов.  3) Для отключяемой проверки, где мы сравниваем информацию базы данных с пользовательским интерфейсом, нам нужно преобразовать список, взятый из базы данных (мы берем только идентификатор и имя).  4) В общей фикстуре "conftest.py" добавили еще одну фикстуру "check_ui".

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,8 +1,9 @@
 from model.group import Group
 import random
+import re
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
      if len(db.get_group_list()) == 0:
           app.group.create(Group(name="test"))
      old_groups = db.get_group_list()
@@ -12,4 +13,11 @@ def test_delete_some_group(app, db):
      assert len(old_groups) - 1 == len(new_groups)
      old_groups.remove(group)
      assert old_groups == new_groups
+     if check_ui:    # for a disabled check, where we compare the database information with the UI,
+                     # we need to transform the list taken from the database (we take only id and name)
+          new_groups_ui = []
+          for i in new_groups:
+               new_groups_ui.append(Group(id=i.id, name=re.sub("  ", " ", i.name.strip())))
+          assert sorted(new_groups_ui, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+#          assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
