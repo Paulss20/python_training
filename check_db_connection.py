aada2cd69d0b0e9b86dde9962e2999612cfd8895
sergei_sh <pazoviy@ayndex.ru>
2020-07-24 13:29:33 +0300

    Урок 7. Видео "8_ПДТ-Python-b12 Отношения между объектами"  Ошибка, при получении списка контактов в группах с индексом "get_contacts_in_group(Group(id="11"))"

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,14 +1,11 @@
 
-#import pymysql.cursors
-#from fixture.db import DbFixture
 from fixture.orm import ORMFixture
-#from model.group import Group
+from model.group import Group
 
-#connection = pymysql.connect(host="127.0.0.1", name="addressbook", user="root", password="")
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-     l = db.get_group_list()
+     l = db.get_contacts_in_group(Group(id="11"))
      for item in l:
           print(item)
      print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -12,43 +12,66 @@ class ORMFixture:
 
     # classes describe the structure that will be written to the database
     class ORMGroup(db.Entity):
-        _table_ = 'group_list'
-        id = PrimaryKey(int, column='group_id')
-        name = Optional(str, column='group_name')
-        header = Optional(str, column='group_header')
-        footer = Optional(str, column='group_footer')
+         _table_ = 'group_list'
+         id = PrimaryKey(int, column='group_id')
+         name = Optional(str, column='group_name')
+         header = Optional(str, column='group_header')
+         footer = Optional(str, column='group_footer')
+         # применяем lanmda, так как класс контакты описан позже, описываем связывающую таблицу и поля
+         # lazy = True - инфорация будет извелкаться только в момент обращения к этому свойству при
+         # получении списка контактов
+         contacts = Set(lambda: ORMFixture.ORMContact, table='address_in_groups',
+                        column='id', reverse='groups', lazy=True)
+
 
     class ORMContact(db.Entity):
-        _table_ = 'addressbook'
-        id = PrimaryKey(int, column='id')
-        firstname = Optional(str, column='firstname')
-        lastname = Optional(str, column='lastname')
-        deprecated = Optional(datetime, column='deprecated')
+         _table_ = 'addressbook'
+         id = PrimaryKey(int, column='id')
+         firstname = Optional(str, column='firstname')
+         lastname = Optional(str, column='lastname')
+         deprecated = Optional(datetime, column='deprecated')
+         groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups',
+                      column='group_id', reverse='contacts', lazy=True)
 
     # binding to physical DB
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
-        self.db.generate_mapping()
-        sql_debug(True)  # shows real sql - query
+         self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+         self.db.generate_mapping()
+         sql_debug(True)  # shows real sql - query
 
     # a list of objects of the "Groups" class is formed
-    def convert_group_to_model(self, groups):
-        def convert(group):
-            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
-        return list(map(convert, groups))
+    def convert_groups_to_model(self, groups):
+         def convert(group):
+              return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+         return list(map(convert, groups))
 
     # need to mark the blocks of code that work with the database session. With (with "db_session:") - in the code
     # or (@db_session :) - before the whole function.
     @db_session
     def get_group_list(self):
-        return self.convert_group_to_model(select(g for g in ORMFixture.ORMGroup))
+         return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
 
     # a list of objects of the "Contacts" class is formed
-    def convert_contact_to_model(self, contacts):
-        def convert(contact):
-            return AddNew(my_id=str(contact.id), my_f_name=contact.firstname, my_l_name=contact.lastname)
-        return list(map(convert, contacts))
+    def convert_contacts_to_model(self, contacts):
+         def convert(contact):
+             return AddNew(my_id=str(contact.id), my_f_name=contact.firstname, my_l_name=contact.lastname)
+         return list(map(convert, contacts))
 
     @db_session
     def get_contact_list(self):
-        return self.convert_contact_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+         orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+         return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+         orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+         return self.convert_contacts_to_model(
+              select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
+
+
+
+
